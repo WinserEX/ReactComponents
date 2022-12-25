9d36a52c8420b30c4ab820d2735f945096fdb6b1
WinserEX <winser500@gmail.com>
2022-12-25 15:47:57 -0400

    Created a method that increases the count of teh counter variable, used both functional and class components

--- vite-project/src/funcComp.jsx
+++ vite-project/src/funcComp.jsx
@@ -1,6 +1,45 @@
 import { Button} from 'react-bootstrap';
 import './funcComp.css'
 import Componente from './Componente';
+import React from 'react';
+import { Component } from 'react';
+
+
+class Contador extends Component{
+    constructor(){
+        super();
+        this.state = {contador: 0}
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(
+            ()=>{
+                return { contador: this.state.contador + 1 }
+            });
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.contador}
+                </h1>
+                <Boton handleClick={ this.handleClick }/>
+            </div>
+        );
+    }
+}
+
+function Boton({ handleClick }){
+    return (
+        <Button           
+             variant="danger"
+             onClick={ ()=> handleClick() }
+             >Click me!
+        </Button>
+    )
+}
 
 function Comp({job, fname, lname}) {
     const estudiantes = [
@@ -13,9 +52,8 @@ function Comp({job, fname, lname}) {
     return (
         <div className='funcComp'>
             { estudiantes.map(est => <Componente {...est}/> ) }
-            <Button           
-             variant="danger">Click me!
-            </Button>
+            
+            <Contador />
         </div>
     )
 }
